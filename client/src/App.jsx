f85acf9756c161e4460003140aaef30d399c91aa
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-08-21 14:47:20 +0530

    Add initial project setup with assets and pages
    - Added various assets including images and SVGs for branding and UI.
    - Created main entry point with React and Tailwind CSS integration.
    - Implemented basic page components: ChatBox, Connections, CreatePost, Discover, Feed, Layout, Login, Messages, Profile.
    - Established CSS styling with Google Fonts and Tailwind CSS.
    - Configured Vite for development environment.

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element = {<Login />}>
+            <Route index element = {<Feed />}></Route>
+            <Route path='messages' element={<Messages />}></Route>
+            <Route path='messages/:userId' element={<ChatBox />}></Route>
+            <Route path = 'connections' element={<Connections />}></Route>
+            <Route path = 'discover' element={<Discover />}></Route>
+            <Route path = 'profile/:profileId' element={<Profile />}></Route>
+            <Route path = 'create-post' element={<CreatePost />}></Route>
+        </Route>
+      </Routes>
+    
+    </>
+  )
+}
+
+export default App
