ba4033fb2aec6dde7e9088b61f3d430338c41fc7
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-08-24 20:27:53 +0530

    Refactor Feed and Layout components to implement loading state and fetch dummy posts data

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../components/Loading';
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
+ const [feeds, setfeeds] = useState([]);
+ const [loading, setLoading] = useState(true)
+
+ const fetchFeeds = async() => {
+  setfeeds(dummyPostsData)
+ }
+
+ useEffect(()=>{
+  fetchFeeds()
+ }, [])
+
+ useEffect(()=>{})
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+
+      {/* stories and post list */}
+      <div>
+        <h1 className='p-4 space-y-6'>Stories</h1>
+        <div>
+          
+        </div>
+      </div>
+
+      {/* right side */}
+      
+    </div>
+  ): <Loading/>
 }
 
 export default Feed
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { Menu, X } from 'lucide-react'
 import { dummyUserData } from '../assets/assets'
 import Loading from '../components/Loading'
@@ -8,27 +8,35 @@ import Loading from '../components/Loading'
 const Layout = () => {
   const user = dummyUserData
   const [sidebarOpen, setSidebarOpen] = React.useState(true)
-  return user ? (
-    <div className='w-full flex h-screen'>
-      <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
 
-      <div className=''>
-        <Outlet/>
+  if (!user) {
+    // Loader always full screen centered
+    return <Loading />
+  }
+
+  return (
+    <div className="w-full flex h-screen relative">
+      <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      <div className="flex-1 overflow-y-auto">
+        <Outlet />
       </div>
-      
 
       {sidebarOpen ? (
-        <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow
-        w-10 h-10 text-gray-600' onClick={() => setSidebarOpen(false)}/>
+        <X
+          className="absolute top-3 right-3 p-2 z-50 bg-white rounded-md shadow
+          w-10 h-10 text-gray-600"
+          onClick={() => setSidebarOpen(false)}
+        />
       ) : (
-        <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={() => setSidebarOpen(true)}/>
+        <Menu
+          className="absolute top-3 right-3 p-2 z-50 bg-white rounded-md shadow
+          w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(true)}
+        />
       )}
-
     </div>
-    
-  ) : (
-    <Loading/>
   )
 }
 
-export default Layout 
+export default Layout
