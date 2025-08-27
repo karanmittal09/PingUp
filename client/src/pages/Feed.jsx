a269bdfe83e872942de900548d2247789054cdc2
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-08-27 15:19:53 +0530

    Add RecentMessages component to display user messages in the Feed

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading';
 import StoriesBar from '../components/StoriesBar';
 import PostCard from '../components/PostCard';
+import RecentMessages from '../components/RecentMessages';
 
 const Feed = () => {
  const [feeds, setfeeds] = useState([]);
@@ -34,10 +35,13 @@ const Feed = () => {
 
       {/* right side */}
       <div className='max-xl:hidden sticky top-0'>
-        <div > 
-          <h1>Sponsered</h1>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'> 
+          <h3 className='text-slate-800 font-semibold '>Sponsered</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+          <p className='text-slate-600'>Email Marketing</p>
+          <p className='text-slate-400'>Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
         </div>
-        <h1>Recent Messages</h1>
+        <RecentMessages/>
       </div>
       
     </div>
